0d3f2f7c36258bee5206d8886817899a7e438ba9
Diego López <diego@Diegos-MacBook-Air-2.local>
2021-12-13 12:56:39 +0100

    Adding the new branch of develop

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to relooooad.
+          Edit <code>src/App.js</code> and save to reloooad.
         </p>
         <a
           className="App-link"
