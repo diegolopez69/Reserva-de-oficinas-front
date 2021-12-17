6567f42b1abfb307c7f418d7d9dc030276a9aad7
Diego López <diego@Diegos-MacBook-Air-2.local>
2021-12-17 08:51:31 +0100

    Adding the screen of Floor Two

--- src/components/screens/FloorTwo/index.js
+++ src/components/screens/FloorTwo/index.js
@@ -6,19 +6,42 @@ import "./index.scss";
 
 export const FloorTwo = () => {
   return (
-    <div className='GeneralFloors'>
+    <div className='GeneralFloorTwo'>
       <div className='HeaderOfFloors'>
         <ArrowBackIosIcon className='IconToGoBack' />
-        <div className='SpaceForTheTitle'>
+        <div className='SpaceForTheTitleOfFloorTwo'>
           <h1 className='HeaderTextOfScreenFloor'>
             Planta 2
           </h1>
         </div>
       </div>
 
-      <div className='BtnOfFloors'>
-        <Button className='BtnOfEachFloor'>
-          Planta 3
+      <div className='GeneralOfBtnOnFloorTwo'>
+        <Button className='EachBtwOnFloorTwo'>
+          2.1
+        </Button>
+        <Button className='EachBtwOnFloorTwo'>
+          2.2
+        </Button>
+        <Button className='EachBtwOnFloorTwo'>
+          2.3
+        </Button>
+        <Button className='EachBtwOnFloorTwo'>
+          2.4
+        </Button>
+      </div>
+      <div className='GeneralOfBtnOnFloorTwo'>
+        <Button className='EachBtwOnFloorTwo'>
+          2.5
+        </Button>
+        <Button className='EachBtwOnFloorTwo'>
+          2.6
+        </Button>
+        <Button className='EachBtwOnFloorTwo'>
+          2.7
+        </Button>
+        <Button className='EachBtwOnFloorTwo'>
+          2.8
         </Button>
       </div>
 
