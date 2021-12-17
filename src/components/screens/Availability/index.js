85e03cf905b7c56ae2ca1773544007f1e4664919
Diego López <diego@Diegos-MacBook-Air-2.local>
2021-12-17 14:18:14 +0100

    Making the request

--- src/components/screens/Availability/index.js
+++ src/components/screens/Availability/index.js
@@ -10,9 +10,11 @@ export const Availability = () => {
 
     useEffect(() => {
       async function fetchData() {
-        const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon/ditto")
-        console.log('respuesta ', data);
-  
+        let stateOfBooking = 0;
+        const { data } = await axios.get("http://172.20.10.3:3000/links/1/Fri&Dec&17&2021/Fri&Dec&17&2021&24:00")
+        if( data.disponibilidad[0] != null)
+        console.log('respuesta ', data.disponibilidad[0]);
+        stateOfBooking = 1;
       }
       fetchData();
     }, [])
