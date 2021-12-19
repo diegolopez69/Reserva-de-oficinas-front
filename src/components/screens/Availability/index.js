90254a77d50988d863d9b2fecc2642a1ae6d7fde
Diego López <diego@Diegos-MacBook-Air-2.local>
2021-12-19 20:43:51 +0100

    Adding the new format to the hours

--- src/components/screens/Availability/index.js
+++ src/components/screens/Availability/index.js
@@ -4,19 +4,20 @@ import { BookingGreen, BookingRed } from "../../organisms";
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import "./index.scss";
 
-
-
-// Dia            
-// Mes            check
-// Dia en fecha   check
-// Año            check
-
-
 export const Availability = () => {
   const [stateOfBooking, setStateOfBooking] = useState(0)
   async function fetchData() {
-    // const {  } = useTime()
-    const { data } = await axios.get("http://172.20.10.3:3000/links/1/Fri&Dec&17&2021&07:00/Fri&Dec&17&2021&24:00")
+    const { min, hour, dayName,  dayNumber, month, year } = Time()
+    let min2 = 0;
+
+    if( min <= 59){
+      min2 = min + 1;
+    }
+
+    const { data } = await axios.get(`http://172.20.10.3:3000/links/1/${dayName}&${month}&${dayNumber}&${year}&${hour}:${min}/${dayName}&${month}&${dayNumber}&${year}&${hour}:${min2}`)
+
+
+    // const { data } = await axios.get("http://172.20.10.3:3000/links/1/Fri&Dec&17&2021&10:00/Fri&Dec&17&2021&24:00")
     //console.log(data.disponibilidad[0])
     if (data.disponibilidad[0] != null) {
       setStateOfBooking(1)
@@ -25,52 +26,95 @@ export const Availability = () => {
     }
   }
   useEffect(() => {
+    setTimeout(() => {
+      
+      fetchData();
+      console.log("Se esta ejecutando");
+    }, 1000);
+    
+
     fetchData();
   }, [])
 
-  // function useTime() {
+  function Time() {
+
+    let today = new Date();
+
+    let day = today.getDay();
+
+    const d = new Date();
+    let min = d.getMinutes();
+    let hour = d.getHours();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
+
+    let dayName = '';
+    if (day === 0) {
+      dayName = 'Sun';
+    } else if (day === 1) {
+      dayName = 'Mon';
+    }
+    else if (day === 2) {
+      dayName = 'Tue';
+    } else if (day === 3) {
+      dayName = 'Wed';
+    }
+    else if (day === 4) {
+      dayName = 'Thu';
+    }
+    else if (day === 5) {
+      dayName = 'Fri';
+    }
+    else if (day === 6) {
+      dayName = 'Sat';
+    }
+
+    if (mm === 0) {
+      mm = 'Jan'
+    }else if(mm = 1 ){
+      mm = 'Feb'
+    }else if(mm = 2 ){
+      mm = 'Mar'
+    }else if(mm = 3 ){
+      mm = 'Apr'
+    }else if(mm = 4 ){
+      mm = 'May'
+    }else if(mm = 5 ){
+      mm = 'Jun'
+    }else if(mm = 6 ){
+      mm = 'Jul'
+    }else if(mm = 7 ){
+      mm = 'Aug'
+    }else if(mm = 8 ){
+      mm = 'Sep'
+    }else if(mm = 9 ){
+      mm = 'Oct'
+    }else if(mm = 10 ){
+      mm = 'Nov'
+    }else if(mm = 11 ){
+      mm = 'Dec'
+    }
     
-  //   var today = new Date();
+    // console.log("min ", min);
+    // console.log("hour ", hour);
+    // console.log("day ", day);
+    // console.log("day ", dayName);
+    // console.log("mm ", mm);
+    // console.log("yy ", yyyy);
+    // console.log("day ", dd);
 
-  //   let day = today.getDay();
+    return {
+      min: min,
+      hour: hour,
+      dayName : dayName,
+      dayNumber: dd,
+      month: mm, 
+      year: yyyy
+    }
+  }
 
-  //   var dd = String(today.getDate()).padStart(2, '0');
-  //   var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-  //   var yyyy = today.getFullYear();
-
-  //   let dayName = '';
-  //   if (day === 1) {
-  //       dayName = 'Sun';
-  //   } else if (day === 2) {
-  //      dayName = 'Mon';
-  //   }
-  //   else if (day === 3) {
-  //      dayName = 'Tue';
-  //   } else if (day === 4) {
-  //      dayName = 'Wed';
-  //   }
-  //   else if (day === 5) {
-  //      dayName = 'Thu';
-  //   }
-  //   else if (day === 6) {
-  //      dayName = 'Fri';
-  //   }
-  //   else if (day === 7) {
-  //      dayName = 'Sat';
-  //   }
-  //   console.log("day ", dayName);
-  //   console.log("mm ", mm);
-  //   console.log("yy ", yyyy);
-  //   console.log("day ", dd);
-
-  //   return {
-  //     dayName : dayName,
-  //     month: mm, 
-  //     year: yyyy, 
-  //     dayNumber: dd
-  //   }
-
-  // }
+  
 
   return (
     <div className='GeneralFloorTwo'>
@@ -82,10 +126,8 @@ export const Availability = () => {
           </h1>
         </div>
       </div>
-      {/* {useTime()} */}
+
       {stateOfBooking === 1 ? <BookingRed /> : <BookingGreen />}
     </div>
   );
 };
-
-
