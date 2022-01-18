9cef5612478a80463b299ee04bb3b1b19cdc6df6
Diego López <diegolopezselva@gmail.com>
2022-01-18 11:45:53 +0100

    Adding the button list but its not implemented yet

--- src/components/screens/BookingMovil/index.js
+++ src/components/screens/BookingMovil/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 import { Link } from "react-router-dom";
 import Input from '@material-ui/core/Input';
 import "./index.scss";
@@ -11,34 +12,40 @@ export const BookingMovil = () => {
       <div className="GeneralHeaderOfBookingMovil">
         <p className="TextOfHeader">Reserva</p>
       </div>
-      <hr />
+
+
+
+
       <div className="GeneralFormOfBookingMovil">
+
         <div className="FormName">
           <p className="TextOfNameOnForm">Diego López</p>
         </div>
 
         <div className="InsideForm">
-          <form>
+          <form className="LabelForm">
             <label>Aula: </label>
             <label>2.1</label>
           </form>
 
-          <form>
+          <form className="LabelForm">
             <label>Correo: </label>
             <label>diego.lopez@alumnos.uneatlantico.es</label>
           </form>
 
-          <form>
+          <form className="LabelForm">
             <label>Hora de entrada: </label>
-            <label>2.1</label>
+            <label>10:00</label>
           </form>
-          
-          <form>
+
+          <form className="LabelForm">
             <label>Hora de salida: </label>
-            <label>2.1</label>
+            <label>15:00</label>
           </form>
         </div>
+
         <div className="BtnToBooking">
+          <Button variant="contained">Reservar</Button>
         </div>
       </div>
     </div>
@@ -54,28 +61,20 @@ export const BookingMovil = () => {
 
 // import React from 'react';
 // import { makeStyles } from '@material-ui/core/styles';
-// import ListSubheader from '@material-ui/core/ListSubheader';
+
 // import List from '@material-ui/core/List';
 // import ListItem from '@material-ui/core/ListItem';
-// import ListItemIcon from '@material-ui/core/ListItemIcon';
 // import ListItemText from '@material-ui/core/ListItemText';
 // import Collapse from '@material-ui/core/Collapse';
-// import InboxIcon from '@material-ui/icons/MoveToInbox';
-// import DraftsIcon from '@material-ui/icons/Drafts';
-// import SendIcon from '@material-ui/icons/Send';
 // import ExpandLess from '@material-ui/icons/ExpandLess';
 // import ExpandMore from '@material-ui/icons/ExpandMore';
-// import StarBorder from '@material-ui/icons/StarBorder';
+
 
 // const useStyles = makeStyles((theme) => ({
 //   root: {
 //     width: '100%',
-//     maxWidth: 360,
-//     backgroundColor: theme.palette.background.paper,
-//   },
-//   nested: {
-//     paddingLeft: theme.spacing(4),
-//   },
+//     maxWidth: 260,
+//   }
 // }));
 
 // export const BookingMovil = () => {
@@ -87,42 +86,21 @@ export const BookingMovil = () => {
 //   };
 
 //   return (
-//     <List
-//       component="nav"
-//       aria-labelledby="nested-list-subheader"
-//       subheader={
-//         <ListSubheader component="div" id="nested-list-subheader">
-//           Nested List Items
-//         </ListSubheader>
-//       }
-//       className={classes.root}
-//     >
-//       <ListItem button>
-//         <ListItemIcon>
-//           <SendIcon />
-//         </ListItemIcon>
-//         <ListItemText primary="Sent mail" />
-//       </ListItem>
-//       <ListItem button>
-//         <ListItemIcon>
-//           <DraftsIcon />
-//         </ListItemIcon>
-//         <ListItemText primary="Drafts" />
-//       </ListItem>
+//     <List component="nav" className={classes.root}>
 //       <ListItem button onClick={handleClick}>
-//         <ListItemIcon>
-//           <InboxIcon />
-//         </ListItemIcon>
-//         <ListItemText primary="Inbox" />
+//         <ListItemText primary="Horas" />
 //         {open ? <ExpandLess /> : <ExpandMore />}
 //       </ListItem>
-//       <Collapse in={open} timeout="auto" unmountOnExit>
+//       <Collapse in={open} >
 //         <List component="div" disablePadding>
-//           <ListItem button className={classes.nested}>
-//             <ListItemIcon>
-//               <StarBorder />
-//             </ListItemIcon>
-//             <ListItemText primary="Starred" />
+//           <ListItem button>
+//             <ListItemText primary="9:00 - 10:00" />
+//           </ListItem>
+//           <ListItem button>
+//             <ListItemText primary="10:00 - 11:00" />
+//           </ListItem>
+//           <ListItem button>
+//             <ListItemText primary="11:00 - 12:00" />
 //           </ListItem>
 //         </List>
 //       </Collapse>
