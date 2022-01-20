c732a1298f16955c3ed30dead019a372b51d6c3c
Diego López <diegolopezselva@gmail.com>
2022-01-20 11:10:20 +0100

    Added the image of Uneat

--- src/components/screens/BookingHome/index.js
+++ src/components/screens/BookingHome/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./index.scss";
 
-
 export const BookingHome = () => {
     return (
         <div className='MainScreenBookingFinished'>
+            
             <p className="TextOfBookingFinished">Booking Home</p>
         </div>
     );
 };
-
