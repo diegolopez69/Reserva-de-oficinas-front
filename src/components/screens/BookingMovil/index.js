415e6b007dd2396d457373c662ca82805ba06872
Diego López <diegolopezselva@gmail.com>
2022-01-25 12:27:50 +0100

    First try to make that the time work

--- src/components/screens/BookingMovil/index.js
+++ src/components/screens/BookingMovil/index.js
@@ -7,7 +7,7 @@ import "./index.scss";
 
 
 export const BookingMovil = () => {
-  // const [open, setOpen] = React.useState(false);
+  const {min, hour, dayNumber, month, year, newFormat} = Time()
   const [value, setValue] = useState("");
   const [valuee, setValuee] = useState("");
   const handleChange = e => setValue(e.target.value);
@@ -20,7 +20,7 @@ export const BookingMovil = () => {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       // body: JSON.stringify({ start_time: value, end_time: valuee, room_id:1, create_by:"admin", name:"Titulo 1", description:"ejemplo1@alumnos.uneatlantico.es" })
-      body: JSON.stringify({ "reserva": { start_time: "2022-01-25T12:08:00", end_time: "2022-01-25T14:08:00", room_id: 1, create_by: "admin", name: "Titulo 1", description: "ejemplo1@alumnos.uneatlantico.es" } })
+      body: JSON.stringify({ "reserva": { start_time: newFormat, end_time: "2022-01-25T14:08:00", room_id: 1, create_by: "admin", name: "Titulo 1", description: "ejemplo1@alumnos.uneatlantico.es" } })
     };
     console.log(requestOptions.body);
     fetch('http://172.20.10.5:3000/links/aceptar_reserva', requestOptions)
@@ -30,13 +30,6 @@ export const BookingMovil = () => {
     // empty dependency array means this effect will only run once (like componentDidMount in classes)
   }
 
-
-
-
-
-
-
-
   function Time() {
     let today = new Date();
     let day = today.getDay();
@@ -45,7 +38,9 @@ export const BookingMovil = () => {
     let dd = String(today.getDate()).padStart(2, '0');
     let mm = String(today.getMonth() + 1).padStart(2, '0');
     let yyyy = today.getFullYear();
-
+    const newFormat = `${yyyy}-${mm}-${dd}T${hour}:${min}:00`;
+    // const newFormat = `${yyyy}-${mm}-${dd}T${hour}:0${min}:00`;
+    
     //2022-01-25T12:00:00
 
     console.log("------------------------");
@@ -58,103 +53,15 @@ export const BookingMovil = () => {
     console.log(`${yyyy}-${mm}-${dd}T${hour}:0${min}:00`);
     console.log("newFormat ", newFormat);
 
-    const newFormat = `${yyyy}-${mm}-${dd}T${hour}:0${min}:00`;
-
-
-    let dayName = '';
-    if (day === 0) {
-      dayName = 'Sun';
-    } else if (day === 1) {
-      dayName = 'Mon';
-    }
-    else if (day === 2) {
-      dayName = 'Tue';
-    } else if (day === 3) {
-      dayName = 'Wed';
-    }
-    else if (day === 4) {
-      dayName = 'Thu';
-    }
-    else if (day === 5) {
-      dayName = 'Fri';
-    }
-    else if (day === 6) {
-      dayName = 'Sat';
-    }
-
-
-    let monthName = '';
-    if (mm === '01') {
-      monthName = 'Jan'
-    } else if (mm === '02') {
-      monthName = 'Feb'
-    } else if (mm === '03') {
-      monthName = 'Mar'
-    } else if (mm === '04') {
-      monthName = 'Apr'
-    } else if (mm === '05') {
-      monthName = 'May'
-    } else if (mm === '06') {
-      monthName = 'Jun'
-    } else if (mm === '07') {
-      monthName = 'Jul'
-    } else if (mm === '08') {
-      monthName = 'Aug'
-    } else if (mm === '09') {
-      monthName = 'Sep'
-    } else if (mm === '10') {
-      monthName = 'Oct'
-    } else if (mm === '11') {
-      monthName = 'Nov'
-    } else if (mm === '12') {
-      monthName = 'Dec'
-    }
-
-
-
-
-    let min2 = 0;
-    min2 = min + 1; //No es necesario validar el 60 porque pone 0, eso resuelve el error
-
     return {
       min: min,
-      min2: min2,
       hour: hour,
-      dayName: dayName,
-      monthName: monthName,
       dayNumber: dd,
       month: mm,
-      year: yyyy
+      year: yyyy,
+      newFormat: newFormat
     }
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
   return (
@@ -162,7 +69,6 @@ export const BookingMovil = () => {
       <div className="GeneralHeaderOfBookingMovil">
         <p className="TextOfHeader">Reserva</p>
       </div>
-      {/* {Time()} */}
       <div className="GeneralFormOfBookingMovil">
 
         <div className="FormName">
