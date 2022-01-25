0820cff5d611e392ed8dc5f804782272b3f8e845
Diego López <diegolopezselva@gmail.com>
2022-01-25 12:14:43 +0100

    Adding the changes to the format of the dat

--- src/components/screens/BookingMovil/index.js
+++ src/components/screens/BookingMovil/index.js
@@ -1,4 +1,4 @@
-import React, { useState  } from "react";
+import React, { useState } from "react";
 import Button from '@material-ui/core/Button';
 import { Select, FormControl, InputLabel, MenuItem } from "@material-ui/core";
 import { useHistory, Link } from "react-router-dom";
@@ -14,21 +14,147 @@ export const BookingMovil = () => {
   const handleChangeOut = e => setValuee(e.target.value);
 
 
-    const PostMethod = () => {
-        // POST request using fetch inside useEffect React hook
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            // body: JSON.stringify({ start_time: value, end_time: valuee, room_id:1, create_by:"admin", name:"Titulo 1", description:"ejemplo1@alumnos.uneatlantico.es" })
-          body: JSON.stringify({ "reserva": {start_time: "2022-01-25T10:00:00", end_time: "2022-01-25T11:00:00", room_id:1, create_by:"admin", name:"Titulo 1", description:"ejemplo1@alumnos.uneatlantico.es" }})
-        };
-        console.log(requestOptions.body);
-        fetch('http://172.20.10.5:3000/links/aceptar_reserva', requestOptions)
-            .then(response => response.json())
-            .then(data => setValue(data.id)).catch(exception => console.error(exception));
+  const PostMethod = () => {
+    // POST request using fetch inside useEffect React hook
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      // body: JSON.stringify({ start_time: value, end_time: valuee, room_id:1, create_by:"admin", name:"Titulo 1", description:"ejemplo1@alumnos.uneatlantico.es" })
+      body: JSON.stringify({ "reserva": { start_time: "2022-01-25T12:08:00", end_time: "2022-01-25T14:08:00", room_id: 1, create_by: "admin", name: "Titulo 1", description: "ejemplo1@alumnos.uneatlantico.es" } })
+    };
+    console.log(requestOptions.body);
+    fetch('http://172.20.10.5:3000/links/aceptar_reserva', requestOptions)
+      .then(response => response.json())
+      .then(data => setValue(data.id)).catch(exception => console.error(exception));
 
     // empty dependency array means this effect will only run once (like componentDidMount in classes)
+  }
+
+
+
+
+
+
+
+
+  function Time() {
+    let today = new Date();
+    let day = today.getDay();
+    let min = today.getMinutes();
+    let hour = today.getHours();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0');
+    let yyyy = today.getFullYear();
+
+    //2022-01-25T12:00:00
+
+    console.log("------------------------");
+    // console.log("Today ", today);
+    // console.log("min ", min);
+    // console.log("hour ", hour);
+    // console.log("Day Number ", dd);
+    // console.log("Month ", mm);
+    // console.log("Year ", yyyy);
+    console.log(`${yyyy}-${mm}-${dd}T${hour}:0${min}:00`);
+    console.log("newFormat ", newFormat);
+
+    const newFormat = `${yyyy}-${mm}-${dd}T${hour}:0${min}:00`;
+
+
+    let dayName = '';
+    if (day === 0) {
+      dayName = 'Sun';
+    } else if (day === 1) {
+      dayName = 'Mon';
     }
+    else if (day === 2) {
+      dayName = 'Tue';
+    } else if (day === 3) {
+      dayName = 'Wed';
+    }
+    else if (day === 4) {
+      dayName = 'Thu';
+    }
+    else if (day === 5) {
+      dayName = 'Fri';
+    }
+    else if (day === 6) {
+      dayName = 'Sat';
+    }
+
+
+    let monthName = '';
+    if (mm === '01') {
+      monthName = 'Jan'
+    } else if (mm === '02') {
+      monthName = 'Feb'
+    } else if (mm === '03') {
+      monthName = 'Mar'
+    } else if (mm === '04') {
+      monthName = 'Apr'
+    } else if (mm === '05') {
+      monthName = 'May'
+    } else if (mm === '06') {
+      monthName = 'Jun'
+    } else if (mm === '07') {
+      monthName = 'Jul'
+    } else if (mm === '08') {
+      monthName = 'Aug'
+    } else if (mm === '09') {
+      monthName = 'Sep'
+    } else if (mm === '10') {
+      monthName = 'Oct'
+    } else if (mm === '11') {
+      monthName = 'Nov'
+    } else if (mm === '12') {
+      monthName = 'Dec'
+    }
+
+
+
+
+    let min2 = 0;
+    min2 = min + 1; //No es necesario validar el 60 porque pone 0, eso resuelve el error
+
+    return {
+      min: min,
+      min2: min2,
+      hour: hour,
+      dayName: dayName,
+      monthName: monthName,
+      dayNumber: dd,
+      month: mm,
+      year: yyyy
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
   return (
@@ -36,7 +162,7 @@ export const BookingMovil = () => {
       <div className="GeneralHeaderOfBookingMovil">
         <p className="TextOfHeader">Reserva</p>
       </div>
-
+      {/* {Time()} */}
       <div className="GeneralFormOfBookingMovil">
 
         <div className="FormName">
