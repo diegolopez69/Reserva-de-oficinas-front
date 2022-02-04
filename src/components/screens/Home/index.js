bcd39cb5e0bd8af43f232bb2022aab52bab2dee7
Diego López <diegolopezselva@gmail.com>
2022-02-04 13:31:27 +0100

    Format the code

--- src/components/screens/Home/index.js
+++ src/components/screens/Home/index.js
@@ -12,8 +12,6 @@ export const Home = () => {
       <div className='TitleOfTheMainScreen'>
         <h2 className='TextOfTheTitleOnMainScreen'>Sistema de reservas de aulas.</h2>
       </div>
-
-
       <div className='GeneralCircleOfMainScreen'>
         <div className="Circle">
           <img src={logoUneat} className="ImgLogoMovil"></img>
@@ -24,7 +22,6 @@ export const Home = () => {
               </Button>
             </Link>
           </div>
-
         </div>
       </div>
     </div>
