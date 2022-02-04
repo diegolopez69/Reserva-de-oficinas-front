d4a4d973ed4353193d5d9fbc94ce9099eaf3e4df
Diego López <diegolopezselva@gmail.com>
2022-02-04 14:02:00 +0100

    Fixing the responsive of the screen of login

--- src/components/screens/BookingHome/index.js
+++ src/components/screens/BookingHome/index.js
@@ -5,10 +5,19 @@ import "./index.scss";
 
 export const BookingHome = () => {
     return (
-        <div className='MainScreenBookingFinished'>
-            <img src={logoUneat} className="ImgLogoMovil"></img>
-            <LoginGoogle/>
-            <br/>
+        <div className='MainScreenLogin'>
+            <br />
+            <div className='TitleOfTheMainScreen'>
+                <h2 className='TextOfTheTitleOnMainScreen'>Sistema de reservas de aulas.</h2>
+            </div>
+            <div className='GeneralCircleOfMainScreen'>
+                <div className="Circle">
+                    <img src={logoUneat} className="ImgLogoMovil"></img>
+                    <div className="btnEnter">
+                        <LoginGoogle />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/screens/index.js
+++ src/components/screens/index.js
@@ -2,7 +2,6 @@ export * from "./Home/";
 export * from "./Floor/";
 export * from "./FloorTwo/";
 export * from "./Availability/";
-export * from "./LoginMovil";
 export * from "./BookingMovil";
 export * from "./BookingFinished";
 export * from "./BookingHome";
