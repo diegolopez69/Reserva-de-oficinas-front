e416eff144eb997e4a26351552c0197e27f44e80
Diego López <diegolopezselva@gmail.com>
2022-02-04 14:18:00 +0100

    Fixing the responsive design of the screen of booking finished

--- src/components/screens/BookingFinished/index.js
+++ src/components/screens/BookingFinished/index.js
@@ -6,9 +6,17 @@ import "./index.scss";
 export const BookingFinished = () => {
     return (
         <div className='MainScreenBookingFinished'>
-            <CheckCircleIcon className="IconCheck"/>
-            <p className="TextOfBookingFinished">Reserva terminada!</p>
+            <div className='FinishedBooking'>
+                <CheckCircleIcon className="IconCheck" />
+                <p className="TextOfBookingFinished">Reserva realizada</p>
+            </div>
         </div>
+
+
+        // <div className='MainScreenBookingFinished'>
+        //     <CheckCircleIcon className="IconCheck"/>
+        //     <p className="TextOfBookingFinished">Reserva terminada!</p>
+        // </div>
     );
 };
 
