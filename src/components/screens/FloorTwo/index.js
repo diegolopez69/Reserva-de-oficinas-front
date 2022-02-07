b945048cd2b616275b1aafc4bb391d495035b820
Diego López <diegolopezselva@gmail.com>
2022-02-07 13:47:23 +0100

    Adding the new way to change between pages

--- src/components/screens/FloorTwo/index.js
+++ src/components/screens/FloorTwo/index.js
@@ -1,11 +1,23 @@
 import React from "react";
 import Button from '@material-ui/core/Button';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
 import "./index.scss";
 
 
 export const FloorTwo = () => {
+
+  const history = useHistory();
+
+  const RouteChange = () => {
+
+
+    history.push("/floorTwoClass");
+  }
+
+
+
   return (
     <div className='GeneralFloorTwo'>
       <div className='HeaderOfFloors'>
@@ -22,37 +34,33 @@ export const FloorTwo = () => {
       </div>
 
       <div className='GeneralOfBtnOnFloorTwo'>
+        <Button onClick={RouteChange} className='EachBtwOnFloorTwo'>
+          2.1
+        </Button>
 
-
-        <Link to="/floorTwoClass">
-          <Button className='EachBtwOnFloorTwo'>
-            2.1
-          </Button>
-        </Link>
-
-        <Button className='EachBtwOnFloorTwo'>
+        <Button onClick={RouteChange} className='EachBtwOnFloorTwo'>
           2.2
         </Button>
-        <Button className='EachBtwOnFloorTwo'>
+        <Button onClick={RouteChange} className='EachBtwOnFloorTwo'>
           2.3
         </Button>
-        <Button className='EachBtwOnFloorTwo'>
+        <Button onClick={RouteChange} className='EachBtwOnFloorTwo'>
           2.4
         </Button>
       </div>
 
 
       <div className='GeneralOfBtnOnFloorTwo'>
-        <Button className='EachBtwOnFloorTwo'>
+        <Button onClick={RouteChange} className='EachBtwOnFloorTwo'>
           2.5
         </Button>
-        <Button className='EachBtwOnFloorTwo'>
+        <Button onClick={RouteChange} className='EachBtwOnFloorTwo'>
           2.6
         </Button>
-        <Button className='EachBtwOnFloorTwo'>
+        <Button onClick={RouteChange} className='EachBtwOnFloorTwo'>
           2.7
         </Button>
-        <Button className='EachBtwOnFloorTwo'>
+        <Button onClick={RouteChange} className='EachBtwOnFloorTwo'>
           2.8
         </Button>
       </div>
