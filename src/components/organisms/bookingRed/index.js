a4c8cf2e0e83baa8352df80e2fb322ee58c53431
Diego López <diegolopezselva@gmail.com>
2022-03-04 11:01:12 +0100

    Remove the variables of context and adding a IP static just for the moment

--- src/components/organisms/bookingRed/index.js
+++ src/components/organisms/bookingRed/index.js
@@ -15,7 +15,7 @@ export const BookingRed = (props) => {
 
   async function fetchData() {
     //console.log(`http://172.20.10.5:3000/links/who/1/${dayLetter}&${month}&${day}&${year}&${hour}:20/${dayLetter}&${month}&${day}&${year}&${newHour}:05`);
-    const { data } = await axios.get(`${REACT_APP_API_URL}/who/1`)
+    const { data } = await axios.get(`http://172.27.18.169:3000/links/who/1`)
     Time()
     // console.log(data.disponibilidad);
     // console.log(data.disponibilidad.create_by);
