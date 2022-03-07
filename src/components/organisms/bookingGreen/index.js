95a508f63ac6e9afffcb3e619a059ee6a3e91793
Genffersson Caal <gcaal@guaitech.com>
2022-03-07 11:13:19 +0100

    Merge Changes

--- src/components/organisms/bookingGreen/index.js
+++ src/components/organisms/bookingGreen/index.js
@@ -1,35 +1,33 @@
-import React from "react";
-import Grid from '@material-ui/core/Grid';
-import QRCode from "react-qr-code";
-import "./index.scss";
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import QRCode from 'react-qr-code'
+import './index.scss'
 
-export const BookingGreen = (props) => {
-  
-  return (
-    <div className='GeneralFloorTwo'>
-      <Grid container className='GeneralSecondPart'>      
-        <Grid item xs={7} className='GeneralStateOfTheRoom'>
-          <div className='GeneralOfFirstLine'>
-            <h3 className='TextOfFirstLine'>{props.hour}:{props.min} {props.dayName} {props.day}/{props.month}/{props.year}</h3>
-          </div>
-          <div className='GeneralOfSecondLine'>
-            <p className='TextOfSecondLine'>LIBRE</p>
-          </div>
-        </Grid>
-        <Grid item xs={5} className='GeneralBooking'>
-          <div className='TitleOfBooking'>          
-            <h3>Hoy</h3>
-          </div>
-          <div className='BookingPart'> 
-            <p className='TextOfBooking'>¡Reserva ahora mismo!</p>
-            <br/> 
-            REACT_APP_FRONT
-            <QRCode value= {`${process.env.REACT_APP_FRONT}/bookingMovil`}/> 
-          </div>
-        </Grid>
-      </Grid>
-    
-
-    </div>
-  );
-};
+export const BookingGreen = props => {
+    return (
+        <div className='GeneralFloorTwo'>
+            <Grid container className='GeneralSecondPart'>
+                <Grid item xs={7} className='GeneralStateOfTheRoom'>
+                    <div className='GeneralOfFirstLine'>
+                        <h3 className='TextOfFirstLine'>
+                            {props.hour}:{props.min} {props.dayName} {props.day}/{props.month}/{props.year}
+                        </h3>
+                    </div>
+                    <div className='GeneralOfSecondLine'>
+                        <p className='TextOfSecondLine'>LIBRE</p>
+                    </div>
+                </Grid>
+                <Grid item xs={5} className='GeneralBooking'>
+                    <div className='TitleOfBooking'>
+                        <h3>Hoy</h3>
+                    </div>
+                    <div className='BookingPart'>
+                        <p className='TextOfBooking'>¡Reserva ahora mismo!</p>
+                        <br />
+                        <QRCode value={`${process.env.REACT_APP_FRONT}/bookingMovil`} />
+                    </div>
+                </Grid>
+            </Grid>
+        </div>
+    )
+}
